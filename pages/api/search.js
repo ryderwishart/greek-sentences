f5cbc834844e123d8f376a8a38680cb2138878a6
ryderwishart <ryderwishart@gmail.com>
2023-04-20 11:54:02 -0700

    Update search endpoint

--- pages/api/search.js
+++ pages/api/search.js
@@ -2,18 +2,37 @@ import fs from 'fs/promises';
 import path from 'path';
 import { odonnell_corpus, hellenistic_corpus } from '../../public/corpora';
 import metaDataArray from '../../public/data/hellenistic_author_metadata.json';
+import fallbackMetaData from '../../public/data/1bc-1ad-meta.json'
 
 console.log('loading api ...');
 const metaDataDict = metaDataArray.reduce((acc, obj) => {
   acc[`${obj.authorId}_${obj.workId}`] = obj;
   return acc;
 }, {});
+/*
+Fallback metadata example:
+{
+    "author": "AELIANUS Tact. ",
+    "authorId": "0546",
+    "date": "A.D. 1-2",
+    "geo": "A.D. 1-2",
+    "title": "Tactica",
+    "workId": "0546_001",
+    "wordCount": "9,893"
+  },
+*/
 
-const hellenistic_author_ids = new Set(hellenistic_corpus.map(entry => entry.id));
+const fallbackMetaDataDict = fallbackMetaData.reduce((acc, obj) => {
+  acc[obj.workId] = obj;
+  return acc;
+}, {});
+
+
+// const hellenistic_author_ids = new Set(hellenistic_corpus.map(entry => entry.id));
 const odonnell_corpus_pairs = new Set(odonnell_corpus.map(entry => `${entry.authorId}_${entry.workId}`));
 const hellenistic_author_dict = Object.fromEntries(hellenistic_corpus.map(entry => [entry.id, entry]));
-const odonnell_corpus_dict = Object.fromEntries(odonnell_corpus.map(entry => [`${entry.authorId}_${entry.workId}`, entry]));
-const dataPath = '../../public/data/json_sentence_pairs';
+// const odonnell_corpus_dict = Object.fromEntries(odonnell_corpus.map(entry => [`${entry.authorId}_${entry.workId}`, entry]));
+// const dataPath = '../../public/data/json_sentence_pairs';
 
 async function* readFromJsonFiles(searchTerms, useOdonnellCorpusFlag = false) {
   const dataPath = path.join(process.cwd(), 'public/data/json_sentence_pairs');
@@ -35,6 +54,13 @@ async function* readFromJsonFiles(searchTerms, useOdonnellCorpusFlag = false) {
 
       // Retrieve the author object
       const author = hellenistic_author_dict[author_id];
+      let fallbackMetadata;
+      if (!author) {
+        // lookup work id in fallback metadata
+
+        fallbackMetadata = fallbackMetaDataDict[`${author_id}_${work_id}`]
+        console.log({ fallbackMetadata, id: `${author_id}_${work_id}`, hellenistic_author_dict_data: hellenistic_author_dict[author_id] })
+      }
 
       const content = await fs.readFile(path.join(jsonPath, file), 'utf8');
       let objects = [];
@@ -57,7 +83,7 @@ async function* readFromJsonFiles(searchTerms, useOdonnellCorpusFlag = false) {
             const lemmasNoAccentsLower = lemmas.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
             if (tokensNoAccentsLower.includes(termNoAccentsLower) || lemmasNoAccentsLower.includes(termNoAccentsLower)) {
               // Add the author object to the result
-              obj.metadata = { ...metadata, ...author };
+              obj.metadata = { ...metadata, ...fallbackMetaData, author: author || fallbackMetadata ? fallbackMetadata?.name : 'no data' };
               // console.log('found', term, obj);
               yield obj;
               break;
@@ -90,10 +116,16 @@ export default async (req, res) => {
     results.push(item);
   }
 
+  if (results.length === 0) {
+    return res.status(404).json({ error: 'No results found.' });
+  }
+
+  const truncatedResults = results.slice(0, 500 > results.length ? results.length : 500);
+
   res.status(200).json({
     count: results.length,
     // format time as seconds to two decimal places
     time: (Date.now() - start) / 1000,
-    results
+    results: truncatedResults
   });
 };
